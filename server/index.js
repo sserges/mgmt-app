1c9dfb384a50a41b95401d9cada572bebd834d7b
sserges <agounkeserge@gmail.com>
2022-06-09 18:45:49 -0400

    Database Connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
+const colors = require('colors')
 
 require('dotenv').config()
+
+const connectDB = require('./config/db')
 
 const schema = require('./schema/schema')
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// Connect to database
+connectDB()
 
 app.use(
   '/graphql',
