04a3bb82f36c9db7e1b6ff89e41600674bccdddb
sserges <agounkeserge@gmail.com>
2022-06-10 18:51:02 -0400

    Fetch Data From MongoDB

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLList,
 } = require('graphql')
 
-const { projects, clients } = require('../sampleData')
+const Project = require('../models/Project')
+const Client = require('../models/Client')
 
 // Project Type
 const ProjectType = new GraphQLObjectType({
@@ -19,7 +20,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId)
+        return Client.findById(parent.clientId)
       },
     },
   }),
@@ -43,26 +44,26 @@ const RootQuery = new GraphQLObjectType({
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id)
+        return Project.findById(args.id)
       },
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects
+        return Project.find()
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id)
+        return Client.findById(args.id)
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients
+        return Client.find()
       },
     },
   },
