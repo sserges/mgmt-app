87f7566df8500f51fdaee934818ebe10bc088283
sserges <agounkeserge@gmail.com>
2022-06-14 19:19:30 -0400

    Apollo Cache

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,12 +1,28 @@
+/* eslint-disable no-restricted-globals */
 import { FaTrash } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
 
 import { DELETE_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from '../queries/clientQueries'
 
 export default function ClientRow({ client }) {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
-        variables: { id: client.id }
+        variables: { id: client.id },
+        // refetchQueries: [{ query: GET_CLIENTS }]
+        update(cache, { data: { deleteClient } }) {
+            const { clients } = cache.readQuery({ query: GET_CLIENTS })
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: { clients: clients.filter(client => client.id !== deleteClient.id) }
+            })
+        }
     })
+
+    const onClickDeleteBtn = () => {
+        if (confirm('Do you really want to delete this client?')) {
+            deleteClient()
+        }
+    }
 
     return (
         <tr>
@@ -14,7 +30,7 @@ export default function ClientRow({ client }) {
             <td>{client.email}</td>
             <td>{client.phone}</td>
             <td>
-                <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+                <button className="btn btn-danger btn-sm" onClick={onClickDeleteBtn}>
                     <FaTrash />
                 </button>
             </td>
