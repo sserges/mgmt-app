bd90ce03751762eed55e57dceee352b61cbd24f6
sserges <agounkeserge@gmail.com>
2022-07-02 14:55:14 -0400

    Delete Project

---
+++ client/src/components/DeleteProjectButton.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-restricted-globals */
+import { useNavigate } from 'react-router-dom'
+import { FaTrash } from 'react-icons/fa'
+
+import { useMutation } from '@apollo/client'
+
+import { DELETE_PROJECT } from '../mutations/projectMutations'
+import { GET_PROJECTS } from '../queries/projectQueries'
+
+export default function DeleteProjectButton({ projectId }) {
+    const navigate = useNavigate()
+
+    const [deleteProject] = useMutation(DELETE_PROJECT, {
+        variables: { id: projectId },
+        onCompleted: () => navigate('/'),
+        refetchQueries: [{ query: GET_PROJECTS }]
+    })
+
+    const handleDelete = () => {
+        if (confirm('Do you really want to delete this project?')) {
+            deleteProject()
+        }
+    }
+
+
+    return (
+        <div className='d-flex mt-5 ms-auto'>
+            <button className='btn btn-danger m-2' onClick={handleDelete}>
+                <FaTrash className='icon' /> Delete Project
+            </button>
+        </div>
+    )
+}
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -5,6 +5,7 @@ import { GET_PROJECT } from '../queries/projectQueries'
 
 import Spinner from '../components/Spinner'
 import ClientInfo from '../components/ClientInfo'
+import DeleteProjectButton from '../components/DeleteProjectButton'
 
 export default function Project() {
     const { id } = useParams()
@@ -33,6 +34,8 @@ export default function Project() {
                 <p className='lead'>{data.project.status}</p>
 
                 <ClientInfo client={data.project.client} />
+
+                <DeleteProjectButton projectId={data.project.id} />
             </div>)}
         </>
     )
